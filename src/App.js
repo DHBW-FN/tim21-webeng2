12a6a2b6d6239828359ed4896e8ba4e0c0d05625
Tobias Goetz <tobigoetz98@gmail.com>
2022-05-31 17:00:39 +0200

    Added wikipedia lookup function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-
 function App() {
+
+  const [wikipedia, setWikipedia] = useState(["Waiting for article..."])
+
+  function wikipediaLookup(city){
+    fetch(`https://de.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${city}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          setWikipedia(data.query.pages[Object.keys(data.query.pages)[0]].extract)
+        })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
