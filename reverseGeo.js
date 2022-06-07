48f7cc36736e8daa28bce5fb89480dd83cb47cd0
Tobias Goetz <tobigoetz98@gmail.com>
2022-06-07 18:56:57 +0200

    Update reverseGeo.js

--- reverseGeo.js
+++ reverseGeo.js
@@ -1,6 +1,5 @@
 async function reverseGeo(longitude, latitude) {
     const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lon=${lon}&lat=${lat}`);
     const data = await response.json();
-  //console.log(data);
-  return data;
+    return data;
 }
