d31fc30f85890995ca234d5f806fcd160814953e
Tobias Goetz <tobigoetz98@gmail.com>
2022-06-08 20:22:12 +0200

    Removed reverseGeo.js because function moved to WikiBox.jsx

--- reverseGeo.js
+++
@@ -1,5 +0,0 @@
-async function reverseGeo(longitude, latitude) {
-    const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lon=${lon}&lat=${lat}`);
-    const data = await response.json();
-    return data;
-}
