be502a0ae8734f5ffe94e3c5bd6885d9e921cf90
Tobias Goetz <tobigoetz98@gmail.com>
2022-06-13 21:24:01 +0200

    Added dynamic change between swipeToStep on mobile and desktop

--- src/components/WikiBox.jsx
+++ src/components/WikiBox.jsx
@@ -1,14 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
-  Page,
-  Button,
-  Sheet,
-  BlockTitle,
-  List,
-  ListItem,
-  Icon,
+    Page,
+    Button,
+    Sheet,
+    BlockTitle,
+    List,
+    ListItem,
+    Icon
 } from 'framework7-react';
-import Framework7 from 'framework7/types';
+import Framework7 from "framework7";
 
 export default function WikiBox() {
     const [wikipedia, setWikipedia] = useState(["Waiting for Wikipedia..."]);
@@ -28,40 +28,9 @@ export default function WikiBox() {
             .then(city => setAddress(city))
     }
 
-    function wikiSheetContent(){
-        return (
-            <>
-                <div className="sheet-modal-inner">
-                    <div className="sheet-modal-swipe-step">
-                        <div className="display-flex padding justify-content-space-between align-items-center">
-                            <h1>{address.city}:</h1>
-                            <Icon f7='location'></Icon>
-                        </div>
-                        <div className=" text-align-center"></div>
-                    </div>
-                    <div className="page-content" style={{height: "auto", maxHeight: "100vh"}}>
-                        <div className="padding-horizontal padding-bottom">
-                            <List>
-                                <ListItem title="Einwohnerzahl">70.000</ListItem>
-                                <ListItem title="Bundesland">Baden Würtemberg</ListItem>
-                            </List>
-                            <div className="margin-top text-align-center">Swipe up for more details</div>
-                        </div>
-                        <BlockTitle medium className="margin-top">
-                            Wiki
-                        </BlockTitle>
-                        <p>
-                            {wikipedia}
-                        </p>
-                    </div>
-                </div>
-            </>
-        )
-    }
-
     // This runs when the component is first loaded
     useEffect(() => {
-        reverseGeo(9.4650, 47.6567).city
+        reverseGeo(9.4650, 47.6567).city;
     }, [])
 
     // This updates the wikipedia text every time the address changes
@@ -69,37 +38,52 @@ export default function WikiBox() {
         wikipediaLookup(address.city)
     }, [address])
 
+    let sheetProps = {
+        className: "wikibox-sheet",
+        style: {height: 'auto'},
+        backdrop: true,
+        swipeToClose: true,
+        swipeToStep: true,
+        closeByBackdropClick: true,
+        closeOnEscape: true
+    };
+    if (Framework7.device.desktop) {
+        sheetProps.swipeToStep = false;
+    }
+
     return (
         <Page>
-            <Button fill id="press_on_Location_Icon" sheetOpen=".wikibox-sheet" onClick={() => wikipediaLookup(address.city)}>
+            <Button fill id="press_on_Location_Icon" sheetOpen=".wikibox-sheet" onClick={() => {
+                wikipediaLookup(address.city);
+            }}>
                 Press to show info
             </Button>
-            {Framework7.device.desktop ? (
-                    <Sheet
-                        className="wikibox-sheet"
-                        style={{ height: 'auto', maxHeight: '100%','--f7-sheet-bg-color': '#fff' }}
-                        backdrop
-                        swipeToClose
-                        closeOnEscape
-                        closeByBackdropClick
-                    >
-                        {wikiSheetContent()}
+            <Sheet
+                {...sheetProps}
+            >
+                <div className="sheet-modal-inner">
+                    <div className="sheet-modal-swipe-step">
+                        <div className="display-flex padding justify-content-space-between align-items-center">
 
-                    </Sheet>
-                ) :
-                (
-                    <Sheet
-                        className="wikibox-sheet"
-                        style={{ height: 'auto', maxHeight: '100%', '--f7-sheet-bg-color': '#fff' }}
-                        backdrop
-                        swipeToStep
-                        swipeToClose
-                        closeOnEscape
-                        closeByBackdropClick
-                    >
-                        {wikiSheetContent()}
-                    </Sheet>
-                )}
+                            <h1>{address.city}:</h1>
+                            <Icon f7='location'></Icon>
+                        </div>
+                    </div>
+                    <div className="padding-horizontal padding-bottom">
+                        <List>
+                            <ListItem title="Einwohnerzahl">70.000</ListItem>
+                            <ListItem title="Bundesland">Baden Würtemberg</ListItem>
+                        </List>
+                        <div className="margin-top text-align-center">Swipe up for more details</div>
+                    </div>
+                    <BlockTitle medium className="margin-top">
+                        Wiki
+                    </BlockTitle>
+                    <p>
+                        {wikipedia}
+                    </p>
+                </div>
+            </Sheet>
         </Page>
     );
 }
