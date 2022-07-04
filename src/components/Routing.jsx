86abd035cf0cad2862df1246144b7137f83ef04f
Niklas <niklas-holl@gmx.de>
2022-07-04 18:54:26 +0200

    changes

--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -1,27 +1,20 @@
-import { useEffect, useContext } from "react";
+import {useEffect, useContext} from "react";
 import L from "leaflet";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
 import "leaflet-routing-machine";
 import "../css/routing.css";
-import { useMap } from "react-leaflet";
-import { CoordContext } from "../js/Context";
+import {useMap} from "react-leaflet";
+import {CoordContext} from "../js/Context";
 
 const routingControl = L.Routing.control({
     waypoints: [],
     draggableWaypoints: false,
     routeWhileDragging: false,
     autoRoute: true,
-    createMarker: function () { return null; }
-    /*createMarker: function(i, wp, nWps) {
-                if (i === 0 || i === nWps - 1) {
-                    return L.marker(wp.latLng, {icon: L.icon({iconUrl: require("../../public/icons/marker-icon.png")}) });
-                } else {
-                    return L.marker(wp.latLng, {icon: L.icon({iconUrl: require("../../public/icons/marker-icon.png")}) });
-                }
-            }*/
+    createMarker: function () {
+        return null;
+    }
 });
-
-//waypoints: [L.latLng(coord.lat, coord.lng), L.latLng(47.67989, 9.47554)],
 
 export default function Routing() {
     const {coord, setCoord} = useContext(CoordContext);
