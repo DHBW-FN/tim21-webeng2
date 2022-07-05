9c62c9af4ec411f682415b24bdfb1b3ad1622928
Tobias Goetz <tobigoetz98@gmail.com>
2022-07-05 21:31:14 +0200

    Feature/i79 rechtschreibfehlerbehebung (#95)
    * Fixed typos for adress to address
    
    * Removed unused variables and imports

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,20 +5,20 @@ import PlacesAutocomplete, {
         getLatLng} 
     from "react-places-autocomplete";
 import { CoordContext } from "../js/Context";
-import { AdressContext } from "../js/Context";
+import { AddressContext } from "../js/Context";
 
 export default function SearchBar() {
-    const { coord, setCoord } = useContext(CoordContext);
-    const { adress, setAdress} = useContext(AdressContext);
+    const { setCoord } = useContext(CoordContext);
+    const { setAddress} = useContext(AddressContext);
     
-    //this way the global adress only gets set when the user makes a selection    
+    //this way the global address only gets set when the user makes a selection
     const [searchAddress, setsearchAddress] = useState("");
    
     const handleSelect = async value => {
         const results = await geocodeByAddress(value);
         const latLng = await getLatLng(results[0]);
         setCoord(latLng);
-        setAdress(value);
+        setAddress(value);
         setsearchAddress(value);
     };
 
@@ -30,7 +30,7 @@ export default function SearchBar() {
         <PlacesAutocomplete value={searchAddress} onChange={setsearchAddress} onSelect={handleSelect}>
             {({getInputProps, suggestions, getSuggestionItemProps, loading}) => (
             <div id="innerSearchdiv">
-                <input id="searchInput" {...getInputProps({ placeholder: "Type adress ..."})} />
+                <input id="searchInput" {...getInputProps({ placeholder: "Type address ..."})} />
                 <div id="autocompletion-examples">
                     {loading ? <div>...loading</div> : null}
 
--- src/js/Context.js
+++ src/js/Context.js
@@ -3,4 +3,4 @@
 import { createContext } from 'react';
 
 export const CoordContext = createContext({});
-export const AdressContext = createContext({});
+export const AddressContext = createContext({});
