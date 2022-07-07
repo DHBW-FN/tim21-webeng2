4fde7b54bf0f7e86d2d034b6a13fb455e6bcc0f0
Tobias Goetz <tobigoetz98@gmail.com>
2022-07-07 19:26:00 +0200

    Reworked wikibox init

--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -1,27 +1,27 @@
-import { useContext } from "react";
-import L from "leaflet";
-import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
-import "leaflet-routing-machine";
-import "../css/routing.css";
-import { useMap } from "react-leaflet";
-import {START_LOCATION, TargetLocation } from "../js/Context";
+import { useContext } from 'react';
+import L from 'leaflet';
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
+import 'leaflet-routing-machine';
+import '../css/routing.css';
+import { useMap } from 'react-leaflet';
+import { START_LOCATION, TargetLocation } from '../js/Context';
 
 const routingControl = L.Routing.control({
-    waypoints: [],
-    draggableWaypoints: false,
-    routeWhileDragging: false,
-    autoRoute: true,
-    createMarker: function () {
-        return null;
-    }
+  waypoints: [],
+  draggableWaypoints: false,
+  routeWhileDragging: false,
+  autoRoute: true,
+  createMarker: function () {
+    return null;
+  }
 });
 
 export default function Routing() {
-    const { targetLocation } = useContext(TargetLocation);
-    const map = useMap();
-    if (!map) return;
-    routingControl.spliceWaypoints(0, 1, START_LOCATION); // -> Start LatLng
-    routingControl.spliceWaypoints(1, 1, L.latLng(targetLocation.lat, targetLocation.lng)); // -> Target LatLng
-    routingControl.addTo(map);
-    return null;
+  const { targetLocation } = useContext(TargetLocation);
+  const map = useMap();
+  if (!map) return;
+  routingControl.spliceWaypoints(0, 1, START_LOCATION); // -> Start LatLng
+  routingControl.spliceWaypoints(1, 1, L.latLng(targetLocation.lat, targetLocation.lng)); // -> Target LatLng
+  routingControl.addTo(map);
+  return null;
 }
