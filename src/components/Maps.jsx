6aac255edb0855222f5225c90152218e8fc7fa2f
philippkuest <100705472+philippkuest@users.noreply.github.com>
2022-07-08 11:53:46 +0000

    Feature/i98 change icon of locate & navigate (#100)
    * Changed icons
    
    * Changed icons
    
    * changed location icon
    
    * implemented different icon for route
    
    * Changed scaling of navigate image
    
    * Changed icon from png to svg
    
    * Removed unused css file
    
    * Reworked icons
    
    * Fixed error on first page load and applied code style
    
    Co-authored-by: Tobias Goetz <tobigoetz98@gmail.com>
    Co-authored-by: mario.schwartz <mario.schwartz2002@gmail.com>

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,67 +1,82 @@
-import React, { useContext } from "react";
-import {MapContainer, TileLayer, useMapEvents, useMap, ZoomControl} from "react-leaflet";
-import "../css/leaflet.css";
-import "../css/app.css";
-import {Fab, Icon, PageContent} from "framework7-react";
+import React, { useContext } from 'react';
+import { MapContainer, TileLayer, useMapEvents, useMap, ZoomControl } from 'react-leaflet';
+import '../css/leaflet.css';
+import '../css/app.css';
+import '../css/maps.css';
+import { Fab, PageContent, Icon } from 'framework7-react';
 import { CoordContext } from '../js/Context';
 
-export default function Map(){
+export default function Map() {
+  const locateFabClickEvent = new Event('handleFabClick');
+  const { coord, setCoord } = useContext(CoordContext);
 
-    const locateFabClickEvent = new Event('handleFabClick');
-    const {coord, setCoord} = useContext(CoordContext)
+  function HandleClick() {
+    const map = useMap();
+    map.on('click', (e) => {
+      setCoord(e.latlng);
+    });
+  }
 
-    function HandleClick(){
-        const map = useMap()
-        map.on("click", (e) => {
-            setCoord(e.latlng)
-        })
-    }
-
-    function HandleFabClick(){
-        const map = useMap();
-        addEventListener('handleFabClick', function () {map.locate()}, false);
-    }
-
-    function EventHandler() {
-        const map = useMapEvents({
-            locationfound(e) {
-                setCoord(e.latlng)
-                map.flyTo(e.latlng, 15)
-            },
-            locationerror() {
-                alert("Unfortunately, we could not find your location")
-            }
-        })
-        return null
-    }
-
-    function FlyToAddress(){
-        const map = useMap()
-        if(coord.lat != null && coord.lng != null){
-            map.flyTo(coord)
-        }
-    }
-
-    return (
-        <>
-            <Fab position="right-bottom" slot="fixed" id="fab-button" onClick={() => {
-                dispatchEvent(locateFabClickEvent);
-            }}>
-                <Icon f7="placemark_fill" ios="f7:placemark_fill" aurora="f7:placemark_fill"></Icon>
-            </Fab>
-            <PageContent className='page-content-map'>
-                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} id="map" zoomControl={false}>
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <ZoomControl position="bottomleft"/>
-                    <HandleClick/>
-                    <HandleFabClick/>
-                    <EventHandler/>
-                    <FlyToAddress/>
-                </MapContainer>
-            </PageContent>
-        </>
+  function HandleFabClick() {
+    const map = useMap();
+    addEventListener(
+      'handleFabClick',
+      function () {
+        map.locate();
+      },
+      false
     );
+  }
+
+  function EventHandler() {
+    const map = useMapEvents({
+      locationfound(e) {
+        setCoord(e.latlng);
+        map.flyTo(e.latlng, 15);
+      },
+      locationerror() {
+        alert('Unfortunately, we could not find your location');
+      }
+    });
+    return null;
+  }
+
+  function FlyToAddress() {
+    const map = useMap();
+    if (coord.lat != null && coord.lng != null) {
+      map.flyTo(coord);
+    }
+  }
+
+  return (
+    <>
+      <Fab
+        position="right-bottom"
+        slot="fixed"
+        id="locateButton"
+        onClick={() => {
+          dispatchEvent(locateFabClickEvent);
+        }}>
+        <Icon id="locateIcon" material="gps_not_fixed" />
+      </Fab>
+      <PageContent className="page-content-map">
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={true}
+          id="map"
+          zoomControl={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <ZoomControl position="bottomleft" />
+          <HandleClick />
+          <HandleFabClick />
+          <EventHandler />
+          <FlyToAddress />
+        </MapContainer>
+      </PageContent>
+    </>
+  );
 }
--- src/components/WikiBox.jsx
+++ src/components/WikiBox.jsx
@@ -1,90 +1,101 @@
-import React, {useState, useEffect, useContext} from 'react';
-import {
-    Sheet,
-    BlockTitle,
-    Icon,
-    Fab,
-    f7
-} from 'framework7-react';
-import Framework7 from "framework7";
-import {$} from "dom7";
+import React, { useState, useEffect, useContext } from 'react';
+import { Sheet, BlockTitle, Fab, f7, Button, Icon } from 'framework7-react';
+import Framework7 from 'framework7';
+import { $ } from 'dom7';
+import '../css/app.css';
+import '../css/wikibox.css';
 import { CoordContext } from '../js/Context';
 
 export default function WikiBox() {
-    const [wikipedia, setWikipedia] = useState(["Waiting for Wikipedia..."]);
-    const [address, setAddress] = useState(["Waiting for city..."]);
-    const {coord} = useContext(CoordContext)
+  const [wikipedia, setWikipedia] = useState(['Waiting for Wikipedia...']);
+  const [address, setAddress] = useState(['Waiting for city...']);
+  const { coord } = useContext(CoordContext);
 
-    async function wikipediaLookup(city){
-        return await fetch(`https://en.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${city}`)
-            .then(response => response.json())
-            .then(data => data.query.pages[Object.keys(data.query.pages)[0]].extract)
-            .then(data => data !== undefined ? setWikipedia(data) : () => {console.warn("No Wikipedia found for " + city); setWikipedia(["No Wikipedia article exists for " + city])})
-    }
-
-    async function reverseGeo(latitude, longitude) {
-        let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyCZXol-ZruQJH-gc_eqlf2RAR4H7VRtaIQ`)
-        let json = await response.json()
-        for (const component of json.results[0].address_components) {
-            if (component.types.includes("locality") || component.types.includes("postal_town")){
-                setAddress(component.long_name)
-                return component.long_name
+  async function wikipediaLookup(city) {
+    return await fetch(
+      `https://en.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${city}`
+    )
+      .then((response) => response.json())
+      .then((data) => data.query.pages[Object.keys(data.query.pages)[0]].extract)
+      .then((data) =>
+        data !== undefined
+          ? setWikipedia(data)
+          : () => {
+              console.warn('No Wikipedia found for ' + city);
+              setWikipedia(['No Wikipedia article exists for ' + city]);
             }
-        }
-        console.warn("No city found for " + latitude + " " + longitude)
-    }
+      );
+  }
 
-    // This updates the wikipedia text every time the address changes
-    useEffect(() => {
-        wikipediaLookup(address)
-    }, [address])
-
-    //This runs when CoordContext changes
-    useEffect(() => {
-        if (coord.lat != null && coord.lng != null){
-            reverseGeo(coord.lat, coord.lng).city
-        }
-    }, [coord])
-
-    let sheetProps = {
-        className: "wikibox-sheet",
-        style: {height: 'auto'},
-        backdrop: true,
-        swipeToClose: true,
-        swipeToStep: true,
-        closeByBackdropClick: true,
-        closeOnEscape: true
-    };
-    if (Framework7.device.desktop) {
-        sheetProps.swipeToStep = false;
-    }
-
-    return (
-        <>
-            <Fab position='center-top' id="debug-fab-open-wikibox" text="Press to show info" onClick={() => f7.sheet.open($('.wikibox-sheet'))}>
-            </Fab>
-            <Sheet
-                {...sheetProps}
-            >
-                <div className="sheet-modal-inner">
-                    <div className="sheet-modal-swipe-step">
-                        <div className="display-flex padding justify-content-space-between align-items-center">
-
-                            <h1>{address}</h1>
-                            <Icon f7='location'></Icon>
-                        </div>
-                    </div>
-                    <div className="padding-horizontal padding-bottom">
-                        <div className="margin-top text-align-center">Swipe up for more details</div>
-                    </div>
-                    <BlockTitle medium className="margin-top">
-                        Wiki
-                    </BlockTitle>
-                    <p>
-                        {wikipedia}
-                    </p>
-                </div>
-            </Sheet>
-        </>
+  async function reverseGeo(latitude, longitude) {
+    let response = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyCZXol-ZruQJH-gc_eqlf2RAR4H7VRtaIQ`
     );
+    let json = await response.json();
+    for (const component of json.results[0].address_components) {
+      if (component.types.includes('locality') || component.types.includes('postal_town')) {
+        setAddress(component.long_name);
+        return component.long_name;
+      }
+    }
+    console.warn('No city found for ' + latitude + ' ' + longitude);
+  }
+
+  // This updates the wikipedia text every time the address changes
+  useEffect(() => {
+    wikipediaLookup(address);
+  }, [address]);
+
+  //This runs when CoordContext changes
+  useEffect(() => {
+    if (coord.lat != null && coord.lng != null) {
+      reverseGeo(coord.lat, coord.lng).city;
+    }
+  }, [coord]);
+
+  let sheetProps = {
+    className: 'wikibox-sheet',
+    style: { height: 'auto' },
+    backdrop: true,
+    swipeToClose: true,
+    swipeToStep: true,
+    closeByBackdropClick: true,
+    closeOnEscape: true
+  };
+  if (Framework7.device.desktop) {
+    sheetProps.swipeToStep = false;
+  }
+
+  return (
+    <>
+      <Fab
+        position="center-top"
+        id="debug-fab-open-wikibox"
+        text="Press to show info"
+        onClick={() => f7.sheet.open($('.wikibox-sheet'))}></Fab>
+      <Sheet {...sheetProps}>
+        <div className="sheet-modal-inner">
+          <div className="sheet-modal-swipe-step">
+            <div className="display-flex padding justify-content-space-between align-items-center">
+              <h1>{address}</h1>
+              <Button id="navigateButton" tooltip={'Navigate to ' + address}>
+                <Icon
+                  id="navigateIcon"
+                  material="directions"
+                  size={$('#navigateButton').height()}
+                />
+              </Button>
+            </div>
+          </div>
+          <div className="padding-horizontal padding-bottom">
+            <div className="margin-top text-align-center">Swipe up for more details</div>
+          </div>
+          <BlockTitle medium className="margin-top">
+            Wiki
+          </BlockTitle>
+          <p>{wikipedia}</p>
+        </div>
+      </Sheet>
+    </>
+  );
 }
