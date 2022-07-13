185e6180893d1648a0a04e82ea5787cc8fd3dad7
Tobias Goetz <tobigoetz98@gmail.com>
2022-07-13 11:25:30 +0200

    Added a few more features to marker

--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -6,24 +6,31 @@ import '../css/routing.css';
 import { useMap } from 'react-leaflet';
 import { DEFAULT_DESTINATION, DestinationContext } from '../js/Context';
 
-const routingControl = L.Routing.control({
-  waypoints: [],
-  draggableWaypoints: false,
-  routeWhileDragging: false,
-  autoRoute: true,
-  createMarker: function (i, wp) {
-    return L.marker(wp.latLng, {
-      draggable: false,
-      icon: L.icon({
-        iconSize: [25, 41],
-        iconAnchor: [13, 41],
-        iconUrl: 'icons/favicon.png'
-      })
-    });
-  }
-});
-
 export default function Routing() {
+  const routingControl = L.Routing.control({
+    waypoints: [],
+    draggableWaypoints: false,
+    routeWhileDragging: false,
+    autoRoute: true,
+    createMarker: function (i, wp) {
+      return L.marker(wp.latLng, {
+        draggable: false,
+        icon: L.icon({
+          iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+          iconSize: [25, 41],
+          iconAnchor: [12, 41],
+          popupAnchor: [1, -34],
+          shadowSize: [41, 41],
+          shadowAnchor: [12, 41],
+        })
+      }).on('click', function (e) {
+        if (e.originalEvent.ctrlKey) {
+          this.remove();
+        }
+      });
+    }
+  });
+
   const { destination } = useContext(DestinationContext);
   const map = useMap();
   if (!map) return;
