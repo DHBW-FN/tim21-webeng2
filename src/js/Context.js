b82c0c4a0f9ac25a0b1af4e8e7ca9d9dfca09639
Tobias Goetz <tobigoetz98@gmail.com>
2022-07-17 10:57:31 +0200

    Added comments to all files in js folder

--- src/js/Context.js
+++ src/js/Context.js
@@ -1,12 +1,21 @@
-//in this file we initalize "global" variables that can be given to any component
-//https://www.youtube.com/watch?v=lnL6gRkQ5g8&ab_channel=PedroTech
+/**
+ * In this file we initialize "global" variables that can be given to any component
+ * like described at https://www.youtube.com/watch?v=lnL6gRkQ5g8&ab_channel=PedroTech
+ *
+ * We also define default values for global variables
+ */
 import { createContext } from 'react';
 
+// Create variables for our context objects
 export const DestinationContext = createContext({});
 export const OriginContext = createContext({});
 export const UserSettingsContext = createContext({});
 export const CenterLocationContext = createContext({});
 
+/**
+ * This is the default Destination
+ * @type {{address: {country: string, city: string, streetNumber: string, street: string}, coordinates: {lng: number, lat: number}}}
+ */
 export const DEFAULT_DESTINATION = {
   coordinates: {
     lat: 47.65673289999999,
@@ -19,6 +28,11 @@ export const DEFAULT_DESTINATION = {
     streetNumber: '21'
   }
 };
+
+/**
+ * This is the default Origin
+ * @type {{address: {country: string, city: string, streetNumber: string, street: string}, coordinates: {lng: number, lat: number}}}
+ */
 export const DEFAULT_ORIGIN = {
   coordinates: {
     lat: 47.65673289999999,
@@ -31,7 +45,17 @@ export const DEFAULT_ORIGIN = {
     streetNumber: '21'
   }
 };
+
+/**
+ * This is the default wiki text
+ * @type {string}
+ */
 export const DEFAULT_WIKI = "No Wikipedia found for this destination :(";
+
+/**
+ * This is the default User Settings
+ * @type {{language: {name: string, options: string[], value: string}, darkMode: {name: string, value: boolean}}}
+ */
 export const DEFAULT_USER_SETTINGS = {
   language: {
     name: 'Language',
